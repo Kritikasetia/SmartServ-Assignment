af6d023d0cd3453b673e96cf19fa7a6d97c3b0fb
Kritikasetia <kritikasetia2002@gmail.com>
2023-12-30 19:31:38 +0530

    implement login page as part of task3

---
+++ task3.js
@@ -0,0 +1,22 @@
+function validateLogin() {
+    // Validate username (email format)
+    var usernameInput = document.getElementById("username");
+    var usernameValue = usernameInput.value;
+    var emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+    if (!emailRegex.test(usernameValue)) {
+        alert("Please enter a valid email address for the username.");
+        return;
+    }
+
+    // Validate password
+    var passwordInput = document.getElementById("password");
+    var passwordValue = passwordInput.value;
+    var passwordRegex = /^(?=.*[A-Z])(?=.*\d)[A-Za-z\d@]+$/;
+    if (!passwordRegex.test(passwordValue)) {
+        alert("Password should include at least one uppercase letter, one lowercase letter, one number, and the characters '@'");
+        return;
+    }
+
+    // If validation passes, you can proceed with further actions (e.g., submitting the form)
+    alert("Login successful!");
+}
